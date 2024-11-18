945287e2d7014f55733305bd4af75d1608be773f
BondarevKir <kirillbondarevv@gmail.com>
2024-11-18 18:15:06 +0300

    Update and rename Second.java to Lession_4/Person.java

---
+++ Lession_4/Person.java
@@ -0,0 +1,19 @@
+package Lession_4;
+
+public class Person {
+    public String name;
+    public String position;
+    public String email;
+    public long phoneNumber;
+    public int salary;
+    public int age;
+
+    public Person(String name, String position, String email, long phoneNumber, int salary, int age) {
+        this.name = name;
+        this.email = email;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+}
